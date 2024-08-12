bf2d59c311c3688c3eeb271363001f68df8334af
Emre Şahin <emrebjk942@gmail.com>
2024-08-12 08:33:32 +0300

    4

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -5,5 +5,5 @@ import * as pageController from '../controllers/pageController.js';
 const router = express.Router();
 
 router.route('/').get(pageController.getIndexPage);
-router.route('/about.html').get(pageController.getAboutPage);
+router.route('/about').get(pageController.getAboutPage);
 export default router;
--- routes/photoRoute.js
+++ routes/photoRoute.js
@@ -5,5 +5,6 @@ import * as photoController from '../controllers/photoController.js';
 const router = express.Router();
 
 router.route('/').post(photoController.createPhoto);
+router.route("/").get(photoController.getAllPhotos);
 
 export default router;
