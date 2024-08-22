5e51b6a4b81393d71e446470fae018c35a771bcb
Emre Şahin <emrebjk942@gmail.com>
2024-08-22 14:48:55 +0300

    10

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,9 +1,33 @@
-const authenticateToken=(req,res,next)=>{
-    const autHeader=req.headers["authorization"]
-    console.log("autHeader",autHeader)
-    const token= autHeader && autHeader.split(" ")[1];
-    console.log("token",token);
-}
+import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+const authenticateToken=async(req,res,next)=>{
+    try {
+        const token= req.headers["authorization"] && req.headers["authorization"].split(" ")[1];
+  
+        if(!token){
+        return res.status(401).json({
+            succeded:false,
+            error:"no token available",
+        
+        });}
+        req.user =await User.findById(
+            jwt.verify(token,process.env.JWT_SECRET).userId
+        );
+    
+      
+    
+    next();
+        
+    } catch (error) {
+        res.status(401).json({
+            succeded:false,
+            error:"not authorized"
+        })
+    }
+    
+   
+  
+};
 
 
 
